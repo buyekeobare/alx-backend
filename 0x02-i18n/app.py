98f290b27fad04e8c0d28e3cd1d00c1d0dde1e40
buyekeobare <buyekeobare@gmail.com>
2024-11-06 01:16:32 +0300

    updated

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" doc doc doc """
+""" Display the current time """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 import pytz
 
 
 class Config(object):
-    """doc doc doc"""
+    """Flask Babel configuration"""
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -27,7 +27,7 @@ users = {
 
 @babel.timezoneselector
 def get_timezone() -> str:
-    """doc doc doc"""
+    """Get the timezone for a web page"""
     try:
         if request.args.get("timezone"):
             return pytz.timezone(request.args.get("timezone")).zone
@@ -39,7 +39,7 @@ def get_timezone() -> str:
 
 
 def get_user() -> dict:
-    """doc doc doc"""
+    """Get user based on user id"""
     user_id = request.args.get("login_as")
     if user_id and int(user_id) in users:
         return users[int(user_id)]
@@ -48,13 +48,14 @@ def get_user() -> dict:
 
 @app.before_request
 def before_request() -> None:
-    """doc doc doc"""
+    """Performs some routines before each request's resolution
+    """
     g.user = get_user()
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """doc doc doc"""
+    """Get the locale for a web page"""
     if request.args.get("locale") in app.config["LANGUAGES"]:
         return request.args.get("locale")
     if g.user and g.user.get("locale") in app.config["LANGUAGES"]:
@@ -64,7 +65,7 @@ def get_locale() -> str:
 
 @app.route("/")
 def index() -> str:
-    """doc doc doc"""
+    """Home page"""
     from datetime import datetime
     from flask_babel import format_datetime
 
